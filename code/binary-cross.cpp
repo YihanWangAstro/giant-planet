c29023912d3f177d7002a9a44b1522295f777ec2
YihanWangAstro <yihan.wang.1@stonybrook.edu>
2019-10-01 18:58:02 -0400

    UPDATE

--- code/binary-cross.cpp
+++ code/binary-cross.cpp
@@ -4,11 +4,14 @@ double V_DISPER{30 * space::unit::kms};
 double V_INF{5};
 // double M_DWARF{0.2};
 double AJ{1};
+
+double AS{1};
+
 constexpr double DELTA{1e-4};
 
 using namespace space::orbit;
 
-void mono_single(std::string workdir, size_t idx, size_t sim_num, double m_dwarf) {
+void mono_binary(std::string workdir, size_t idx, size_t sim_num, double m_dwarf) {
   using namespace space;
   std::random_device rd;
   std::mt19937 local_thread_gen(rd());
@@ -20,6 +23,8 @@ void mono_single(std::string workdir, size_t idx, size_t sim_num, double m_dwarf
   // double v_inf = V_INF * space::unit::kms;  // V_RATIO * critical_velocity_of_1_plus_2(sun.mass, jupiter.mass, );
 
   double a_j = AJ * space::unit::au;
+
+  double a_s = AS * space::unit::au;
 
   double m_in = unit::m_solar + unit::m_jupiter;
 
@@ -41,17 +46,24 @@ void mono_single(std::string workdir, size_t idx, size_t sim_num, double m_dwarf
 
     move_particles_to(jupiter_orbit, jupiter);
 
+    move_to_com_coord(sun, jupiter);
+
     double b = sqrt(randomGen::Uniform<double>::get(local_thread_gen, 0, b_max * b_max));
 
     auto [incid_pos, incid_vel, w, incl, phi] = create_incident_M_star(local_thread_gen, u_out, v_inf, b, start_r);
 
-    Particle star{m_dwarf, pow(m_dwarf, 1.0 / 3) * unit::r_solar};
+    auto binary_orbit =
+        Kepler{0.5 * m_dwarf, 0.5 * m_dwarf, semi_latus_rectum(a_s, 0.0), 0.0, 0.0, 0.0, ISOTHERMAL, 0.0};
 
-    move_particles_to(incid_pos, incid_vel, star);
+    Particle star1{0.5 * m_dwarf, pow(0.5 * m_dwarf, 1.0 / 3) * unit::r_solar};
 
-    move_to_com_coord(sun, jupiter);
+    Particle star2{0.5 * m_dwarf, pow(0.5 * m_dwarf, 1.0 / 3) * unit::r_solar};
 
-    move_to_com_coord(sun, jupiter, star);
+    move_particles_to(binary_orbit, star2);
+
+    move_particles_to(incid_pos, incid_vel, star1, star2);
+
+    move_to_com_coord(sun, jupiter, star1, star2);
 
     double end_time = 2 * time_to_pericenter(u_out, v_inf, b, start_r);
 
@@ -64,10 +76,10 @@ void mono_single(std::string workdir, size_t idx, size_t sim_num, double m_dwarf
     args.add_stop_point_operation([&](auto &ptc) {
       // out_file << PACK(i, ' ', jupiter_orbit, ' ', v_inf, ' ', b, ' ', w, ' ', incl, ' ', phi, ' ', ptc, ' ',
       // "\r\n");
-      space::display(out_file, i, jupiter_orbit, v_inf, b, w, incl, phi, ptc, "\r\n");
+      space::display(out_file, i, jupiter_orbit, v_inf, b, w, incl, phi, ptc, binary_orbit, "\r\n");
     });
 
-    spacex::SpaceXsim simulator{0, sun, jupiter, star};
+    spacex::SpaceXsim simulator{0, sun, jupiter, star1, star2};
 
     simulator.run(args);
   }
@@ -78,7 +90,7 @@ int main(int argc, char **argv) {
 
   size_t sim_num;
 
-  space::tools::read_command_line(argc, argv, sim_num, V_INF, AJ, output_name);
+  space::tools::read_command_line(argc, argv, sim_num, AS, AJ, output_name);
 
   size_t thread_num = 80;
 
